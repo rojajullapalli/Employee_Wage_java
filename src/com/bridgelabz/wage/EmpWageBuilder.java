5dc15ed7552309842471a139cae1c1d622ba3e11
rojajullapalli <rojajules@gmail.com>
2021-08-16 13:49:54 +0530

    adding parttime employee and his wage

--- src/com/bridgelabz/wage/EmpWageBuilder.java
+++ src/com/bridgelabz/wage/EmpWageBuilder.java
@@ -1,7 +1,7 @@
 package com.bridgelabz.wage;
 /**
- *EmpWageBuildweUC1 is an application to check whether Employee is present or not.
- * Employee is present then it will calculate the wage of the employee for day.
+ *EmpWageBuildweUC1 is an application to check whether Employee is fulltime or absent or partime.
+ * Based upon Employee is fulltime,absent,parttime then it will calculate the wage of the employee for day.
  *
  * @author : roja
  * @version :1.1
@@ -9,24 +9,32 @@ package com.bridgelabz.wage;
  **/
 public class EmpWageBuilder {
     public static void main(String[] args) {
-        int IS_FULL_TIME = 1; //constant to check employee is full time
-        int empRatePerHr = 20; //constant to how much employee will get paid according to hour
-        int salary; //variable
-        int empHrs; //variable
-        double empcheck = Math.floor(Math.random() * 10) % 2; //it will generate the random number between 0 and 1 and stores it in empcheck
+        int IS_FULL_TIME = 1;//constant to check employee is full time
+        int IS_PART_TIME = 2;//constant to check employee is part time
+        int empRatePerHr = 20;//constant to how much employee will get paid according to hour
+        int salary;//variable
+        int empHrs;//variable
+        ////it will generate the random number between 0 to 2 and stores it in empcheck.
+        double empcheck = Math.floor(Math.random() * 10) % 3;
         /**
-         * if empcheck equal to 1 then it goes to if and print employee is present and his working hours as 8 .
+         * if empcheck equal to 1 then it goes to if and print employee is fulltime and his working hours as 8 .
+         * if empcheck equal to 2 then it goes to else if and print employee is parttime and his working hours as 4.
          * if empcheck is 0 then goes to else employee is absent and his working hours as 0.
          */
-        if (empcheck == IS_FULL_TIME) {
-            System.out.println("Employee is present");
+        if (empcheck ==  IS_FULL_TIME) {
+            System.out.println("Employee is fulltime present");
             empHrs = 8;
-        } else {
+        }
+        else if (empcheck == IS_PART_TIME){
+            System.out.println("Employee is parttime");
+            empHrs = 4;
+        }
+        else {
             System.out.println("Employee is absent");
             empHrs = 0;
         }
-        //his salary(paycheck or wage) of the day is calculated based on present and absent.
+        //his salary(paycheck or wage) of the day is calculated based on fulltime or partime or absent.
         salary = empHrs * empRatePerHr;
-        System.out.println("salary is " + salary);
+        System.out.println("salary is " +salary);
     }
 }
