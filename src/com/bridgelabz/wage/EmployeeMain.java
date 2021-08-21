7ef326d380e8176563455c13a48b552225ce6f62
rojajullapalli <rojajules@gmail.com>
2021-08-21 12:36:20 +0530

    MODIFIED:Ability To Manage Multiple Company Wage Using Interface

--- src/com/bridgelabz/wage/EmployeeMain.java
+++ src/com/bridgelabz/wage/EmployeeMain.java
@@ -1,6 +1,6 @@
 package com.bridgelabz.wage;
 
-public class EmployeeMain {
+public class EmployeeMain implements employee {
 
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
